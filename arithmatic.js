97e942d4c8dfcac592643653e038f4aa00a81a56
Kirti097 <56297212+Kirti097@users.noreply.github.com>
2019-10-10 03:46:31 -0700

    Add files via upload

---
+++ arithmatic.js
@@ -0,0 +1,30 @@
+<html>
+<head>
+<script type="text/javascript">
+var a=33;
+var b=10;
+var c="Test";
+var linebreak="<br/>";
+document.Write("a+b=");
+result=a+b;
+document.Write(result);
+document.Write(linebreak);
+document.Write("a-b=");
+result=a-b;
+document.Write(result);
+document.Write(linebreak);
+document.Write("a*b=");
+result=a*b;
+document.Write(result);
+document.Write(linebreak);
+document.Write("a/b=");
+result=a/b;
+document.Write(result);
+document.Write(linebreak);
+</script>
+</head>
+<body>
+</body>
+</html>
+
+
